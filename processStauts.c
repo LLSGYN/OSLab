7ac5a6043472cb338b1ba558d282a35a76cd8937
“WayneTian-42” <arthassum@gmail.com>
2022-04-06 20:32:16 +0800

    展示进程状态

---
+++ processStauts.c
@@ -0,0 +1,65 @@
+#include "processStauts.h"
+
+void showAllProcess();
+void showSingleProcess(int pid);
+
+void showAllProcess()
+{
+	for (int i = 0; i < MAX_PROCESS; i++)
+	{
+		WaitForSingleObject(allPCB[i].processMutex, INFINITE);
+		if (usedProcessID[i])
+			showSingleProcess(i);
+		ReleaseSemaphore(allPCB[i].processMutex, 1, NULL);
+	}
+}
+
+void showSingleProcess(int pid)
+{
+	PCB pcb = allPCB[pid];
+	printf("\n---------PROCESS %d---------\n", pcb.ID);
+	printf("name: %s\n", pcb.name);
+	if (pcb.fatherProID != -1)
+		printf("fatherProID: %d\n", pcb.fatherProID);
+	printf("priority: %d\n", pcb.priority);
+	printf("nowState: %d\n", pcb.nowState);
+	printf("eventID: %d\n", pcb.eventID);
+	printf("eventTime: %d\n", pcb.eventTime);
+	printf("eventNum: %d\n", pcb.eventNum);
+	printf("all events:\n");
+	for (int i = 0; i < pcb.eventNum; i++)
+	{
+        printf("\tevent %d : type: ", i);
+        Event event = pcb.events[i];
+        switch (event.eventType)
+        {
+          case occupyCPU:
+            printf("occupyCPU  time: %d  needRAM: %d\n", event.time, event.needRAM);
+            break;
+          case occupyIO:
+            printf("occupyIO  time: %d  needRAM: %d  IDofIO: %d\n", event.time, event.needRAM, event.eventMsg.IDOfIO);
+            break;
+          case createProcess:
+            printf("createProcess  time: %d  needRAM: %d\n", event.time, event.needRAM);
+            break;
+          case proReadMem:
+            printf("proReadMemory  time: %d  needRAM: %d  startPageID: %d  pageNum: %d\n", event.time, event.eventMsg.wrMsg.startPageID, event.eventMsg.wrMsg.pageNum, event.needRAM);
+            break;
+          case proWriteMem:
+            printf("proWriteMemory  time: %d  needRAM: %d  startPageID: %d  pageNum: %d\n", event.time, event.eventMsg.wrMsg.startPageID, event.eventMsg.wrMsg.pageNum, event.needRAM);
+            break;
+          case heapAlloc:
+            printf("heapAlloc  time: %d  needRAM: %d  pageNum: %d\n", event.time, event.eventMsg.allocNum, event.needRAM);
+            break;
+          case stackAlloc:
+            printf("stackAlloc  time: %d  needRAM: %d  pageNum: %d\n", event.time, event.eventMsg.allocNum, event.needRAM);
+            break;
+          case compile:
+            printf("compile  time: %d  needRAM: %d  fileName: %s\n", event.time, event.eventMsg.filename, event.needRAM);
+            break;
+          default:
+            printf("event type error!\n");
+            break;
+        }
+	}
+}
---
+++ processStauts.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "dfVar.h"
+
+extern void showAllProcess();
+extern void showSingleProcess(int pid);
