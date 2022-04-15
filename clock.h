5f869457c72ca4951463c403a8779512be4faf83
wgzx6666 <59910479+wgzx6666@users.noreply.github.com>
2022-04-15 20:18:27 +0800

    clock头文件

---
+++ clock.h
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+// #define MAX_PROCESS 16
+// #define NUM_PAGE 4096
+
+typedef struct PageNode //页面信息节点
+{
+    int pageid; //页面id
+    int used;   //使用位 0  标记是否使用过该页
+} pagenode, *pageptr;
+
+typedef struct PageNodeVector
+{
+    int pointer;                        //当前指针指向数组的位置
+    int maxSet;                         //当前此驻留集的页面数
+    pagenode frameVector[NUM_PAGE]; //当前进程可拥有的最大页面数
+} pagevector, *pagevectorptr;
+
+void CreateCLKSet(int processid, int setsize); //将某一进程的驻留集初始化
+void ChangeUsedBit(int processid, int pageid); //将近期使用过的特定页面使用位置为一
+int ReplacePage(int processid, int pageid);//页面替换
+int ResetResidentSet(int processid);//清除一个进程的内存使用信息
+void OutputCLKFrame(int processid); //输出当前驻留集内容
